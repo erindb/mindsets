d7f96574eab797c54b27351b1cd07c326c10baba
erin <festethejester@gmail.com>
2014-05-22 10:03:55 -0700

    results after 60 Ss

--- goals.R
+++ goals.R
@@ -16,13 +16,16 @@ r = read.table("goals-5-21.results", header=T, sep=",")
 print(length(r))
 r = r[r$response < 0.2 | r$version != "test_bad",]
 print(length(r))
-ggplot(r, aes(x=dweck_sum_score)) +
-  geom_histogram(binwidth = 0.1)
+
+
 med.split = median(r$dweck_sum_score)
 r$fixed = r$dweck_sum_score > med.split
 up.quart = median(r$dweck_sum_score[r$dweck_sum_score > med.split])
 low.quart = median(r$dweck_sum_score[r$dweck_sum_score < med.split])
-#r = r[r$dweck_sum_score < low.quart | r$dweck_sum_score > up.quart,]
+r = r[r$dweck_sum_score < low.quart | r$dweck_sum_score > up.quart,]
+
+ggplot(r, aes(x=dweck_sum_score)) +
+  geom_histogram(binwidth = 0.1)
 
 mean.goals = aggregate(response~version+fixed, data=r[r$trial_type == "g",], FUN=mean)
 upper.goals = aggregate(response~version+fixed, data=r[r$trial_type == "g",], FUN=upper.conf)

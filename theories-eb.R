4f8e66c70a388d6960d08b67c995351fc38a34f8
erin <festethejester@gmail.com>
2014-05-29 13:53:28 -0700

    erin's graphing script

--- theories-eb.R
+++ theories-eb.R
@@ -1,3 +1,5 @@
+split = "quartile"
+
 ## Summarizes data.
 ## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
 ##   data: a data frame.
@@ -41,8 +43,11 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
   return(datac)
 }
 
-# read file
-t <- read.csv("theories_results_processed.csv")
+# read filesu
+#t = read.csv("theories69.csv")
+t = read.csv("theories71.csv")
+#t = t[t$workerID < 31,]
+#t <- read.csv("theories_results_processed.csv")
 
 # factors and labels
 t$effort <- factor(t$effort, levels=c("low", "medium", "high"),
@@ -53,7 +58,7 @@ t$difficulty <- factor(t$difficulty, levels=c("easy", "difficult"),
                        labels=c("Easy", "Difficult"))
 
 # filter
-t <- subset(t, heardOf=="no")
+#t <- subset(t, heardOf=="no")
 t <- subset(t, sanity1 > sanity0)
 
 # mindset score
@@ -64,17 +69,30 @@ med.split = median(t$entityScore)
 up.quart = median(t$entityScore[t$entityScore > med.split])
 low.quart = median(t$entityScore[t$entityScore < med.split])
 
-t$mindset <- ifelse(t$entityScore > med.split, "fixed", "growth")
+if (split == "median") {
+  t$mindset <- ifelse(t$entityScore > med.split, "fixed", "growth")
+} else if (split == "quartile") {
+  t$mindset <- ifelse(t$entityScore >= up.quart, "fixed", NA)
+  t$mindset[t$entityScore <= low.quart] = "growth"
+  t = t[!is.na(t$mindset),]
+}
 
-t$effort = as.character(t$effort)
-t$effort[t$effort == "Low effort"] = "0"
-t$effort[t$effort == "Medium effort"] = "0.5"
-t$effort[t$effort == "High effort"] = "1"
-t$effort = as.numeric(t$effort)
+t$e.effort = as.character(t$effort)
+t$e.effort[t$e.effort == "Low effort"] = "0"
+t$e.effort[t$e.effort == "Medium effort"] = "0.5"
+t$e.effort[t$e.effort == "High effort"] = "1"
+t$e.effort = as.numeric(t$e.effort)
 
-t$mindset <- ifelse(t$entityScore >= up.quart, "fixed", NA)
-t$mindset[t$entityScore <= low.quart] = "growth"
-t = t[!is.na(t$mindset),]
+t$e.ability = as.character(t$ability)
+t$e.ability[t$e.ability == "Low ability"] = "0"
+t$e.ability[t$e.ability == "Medium ability"] = "0.5"
+t$e.ability[t$e.ability == "High ability"] = "1"
+t$e.ability = as.numeric(t$e.ability)
+
+t$e.difficulty = as.character(t$difficulty)
+t$e.difficulty[t$e.difficulty == "Easy"] = "0.5"
+t$e.difficulty[t$e.difficulty == "Difficult"] = "2"
+t$e.difficulty = as.numeric(t$e.difficulty)
 
 # mindset histogram
 ggplot(t, aes(x=entityScore)) + 
@@ -128,19 +146,88 @@ ggplot(improvement.all, aes(x=entityScore, y=response, color=effort)) +
   xlab("Fixedness") +
   ylab("Improvement")
 
-ggplot(improvement.all, aes(x=effort, y=response, color=mindset)) +
+ggplot(t, aes(x=e.difficulty, y=response, color=mindset)) +
   geom_point() +
+  facet_grid(. ~ theoryType) +
   #facet_grid(difficulty ~ ability) +
-  theme_bw() +
+  theme_bw(24) +
+  stat_smooth(method="lm") +
+  xlab("difficulty") +
+  ylab("response") +
+  ggtitle("improvement/performace as a function of difficulty")
+#ggtitle("improvement as a function of effort")
+
+ggplot(t, aes(x=e.ability, y=response, color=mindset)) +
+  geom_point() +
+  facet_grid(. ~ theoryType) +
+  #facet_grid(difficulty ~ ability) +
+  theme_bw(24) +
+  stat_smooth(method="lm") +
+  xlab("ability") +
+  ylab("response") +
+  ggtitle("improvement/performace as a function of ability")
+#ggtitle("improvement as a function of effort")
+
+ggplot(t, aes(x=entityScore, y=response, color=effort)) +
+  #geom_point() +
+  facet_grid(. ~ theoryType) +
+  #facet_grid(difficulty ~ ability) +
+  theme_bw(24) +
+  stat_smooth(method="lm") +
+  xlab("Dweck score") +
+  ylab("response") #+
+  #ggtitle("")
+
+ggplot(t, aes(x=e.effort, y=response, color=mindset)) +
+  geom_point() +
+  facet_grid(. ~ theoryType) +
+  #facet_grid(difficulty ~ ability) +
+  theme_bw(24) +
   stat_smooth(method="lm") +
   xlab("effort") +
-  ylab("improvement")
+  ylab("response") +
+  ggtitle("improvement/performace as a function of effort")
+  #ggtitle("improvement as a function of effort")
+# 
+# ggplot(performance.all, aes(x=e.effort, y=response, color=mindset)) +
+#   geom_point() +
+#   #facet_grid(difficulty ~ ability) +
+#   theme_bw(24) +
+#   stat_smooth(method="lm") +
+#   xlab("effort") +
+#   ylab("performance") +
+#   ggtitle("performance as a function of effort")
 
+improvement.fixed = improvement.all[improvement.all$mindset == "fixed",]
+improvement.growth = improvement.all[improvement.all$mindset == "growth",]
+f.fixed = lm(response~e.effort*e.difficulty, data=improvement.fixed)
+#print(anova(f.fixed))
+f.growth = lm(response~e.effort*e.difficulty, data=improvement.growth)
+#print(anova(f.growth))
+# print(f.fixed)
+# print(f.growth)
 
-ggplot(performance.all, aes(x=effort, y=response, color=mindset)) +
-  geom_point() +
-  #facet_grid(difficulty ~ ability) +
-  theme_bw() +
-  stat_smooth(method="lm") +
-  xlab("effort") +
-  ylab("improvement")
+resample.lm = function(df) {
+  rows = sample(1:nrow(df), nrow(df), replace=T)
+  new.df = df[rows,]
+  return(lm(response~e.effort*e.difficulty, data=new.df)$coefficients)
+}
+
+lm.conf = function(df) {
+  resampled.lm = replicate(100, resample.lm(df))
+  intercepts = quantile(resampled.lm["(Intercept)",], c(0.025, 0.975))
+  effort = quantile(resampled.lm["e.effort",], c(0.025, 0.975))
+  difficulty = quantile(resampled.lm["e.difficulty",], c(0.025, 0.975))
+  interaction = quantile(resampled.lm["e.effort:e.difficulty",], c(0.025, 0.975))
+  return(rbind(intercepts, effort, difficulty, interaction))
+}
+g.conf = lm.conf(improvement.growth)
+f.conf = lm.conf(improvement.fixed)
+plot(x=c(1,2,3,4,1,2,3,4), y=c(g.conf), col="blue", pch=19, ylim=c(-0.3,0.5))
+par(new=T)
+plot(x=c(1,2,3,4,1,2,3,4), y=c(f.conf), col="red", pch=19, ylim=c(-0.3,0.5))
+
+f.impr = lm(response~e.effort*e.difficulty*e.ability*entityScore, data=improvement.all)
+f.perf = lm(response~e.effort*e.difficulty*e.ability*entityScore, data=performance.all)
+print(anova(f.impr))
+print(anova(f.perf))

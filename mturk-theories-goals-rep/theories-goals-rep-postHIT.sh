16150c07f213e97606b36bd5ff80e9e91eaab730
erin <festethejester@gmail.com>
2014-07-15 16:11:05 -0700

    restructure directory

--- mturk-theories-goals-rep/theories-goals-rep-postHIT.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-cd /home/feste/opt/aws-mturk-clt-1.3.1/bin
-./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /home/feste/CoCoLab/mindsets/theories-goals-rep/theories-goals-rep -input /home/feste/CoCoLab/mindsets/theories-goals-rep/theories-goals-rep.input -question /home/feste/CoCoLab/mindsets/theories-goals-rep/theories-goals-rep.question -properties /home/feste/CoCoLab/mindsets/theories-goals-rep/theories-goals-rep.properties -maxhits 1
